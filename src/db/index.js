a133342771e5659a3a35ffea5a0428d82dc93d1c
Gabriel Vasconcelos <gabriellrvasconcelos@gmail.com>
2024-01-28 20:23:31 -0300

    Configurando conexão com database

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+// app.js
+const { Pool } = require('pg');
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const db = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    user: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+      rejectUnauthorization: false,
+    },
+  });
+  
+  module.exports = db;
+  
